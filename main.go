475247cdfad74de7206bf6d5af1f18e2a37536d4
James Raden <james.raden@gmail.com>
2022-01-22 14:57:29 -0500

    WIP. The article doesn't explain where to paste this code

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Setting up the server!")
+
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("upgrade failed: ", err)
+			return
+		}
+		defer conn.Close()
+
 	})
 	http.ListenAndServe(":8080", nil)
 }
